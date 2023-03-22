20f6a2a8ec685bbaac3317e09255e0fe9bc369aa
Tinnapop13 <tinnapoptienbang@gmail.com>
2023-03-22 11:22:14 +0700

    second command

--- MyProject1.java
+++ MyProject1.java
@@ -3,6 +3,7 @@ package myproject1;
 public class MyProject1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Both");
     }
     
 }
